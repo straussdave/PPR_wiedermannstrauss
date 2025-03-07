16485de7537c883831e864c2ce57885c726a6cbc
straussdave <davidstrauss002@gmai.com>
2025-03-07 09:19:14 +0100

    Mandelbrot setup

---
+++ Mandelbrot/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+namespace Mandelbrot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
