a9c74fb4959ee19d68d6f12144a190efdf9a2468
straussdave <davidstrauss002@gmai.com>
2025-03-15 15:40:45 +0100

    data evaluated

--- Quicksort In Place Parallel/Program.cs
+++ Quicksort In Place Parallel/Program.cs
@@ -4,6 +4,24 @@ namespace Quicksort_In_Place_Parallel
 {
     internal class Program
     {
+        static async Task Main(string[] args)
+        {
+            int arraySize = 10000000;
+            //Array anlegen und befüllen
+            int[] array = new int[arraySize];
+            Random rnd = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(1, arraySize * 10);
+            }
+
+            //BenchmarkStatic(array);
+            //await BenchmarkNaiveParallel(array);
+            await BenchmarkThresholdApproachAsync(array);
+
+            Console.WriteLine("DONE");
+        }
+
         private static void swap(int[] array, int i, int j)
         {
             int tmp = array[i];
@@ -11,7 +29,7 @@ namespace Quicksort_In_Place_Parallel
             array[j] = tmp;
         }
 
-        private static void AsyncQuicksort(int[] array, int left, int right)
+        private static async Task AsyncQuicksortAsync(int[] array, int left, int right)
         {
             if (left >= right)
             {
@@ -35,12 +53,15 @@ namespace Quicksort_In_Place_Parallel
                     j--;
                 }
             }
-            if (left < j)
-                Parallel.Invoke(() =>AsyncQuicksort(array, left, j));
-            if (i < right)
-                Parallel.Invoke(() =>AsyncQuicksort(array, i, right));
+
+            // Erstelle Tasks für linke und rechte Partition
+            Task leftTask = (left < j) ? Task.Run(() => AsyncQuicksortAsync(array, left, j)) : Task.CompletedTask;
+            Task rightTask = (i < right) ? Task.Run(() => AsyncQuicksortAsync(array, i, right)) : Task.CompletedTask;
+
+            // Warte auf beide Tasks
+            await Task.WhenAll(leftTask, rightTask);
         }
-        private static void ThresholdQuicksort(int[] array, int left, int right, int threshold)
+        private static async Task ThresholdQuicksortAsync(int[] array, int left, int right, int threshold)
         {
             if (left >= right)
             {
@@ -64,19 +85,22 @@ namespace Quicksort_In_Place_Parallel
                     j--;
                 }
             }
-            
-            if (left < j)
-                if ((j-left) < threshold)
-                    SyncQuicksort(array, left, j);
-                else
-                    //Task.Factory.StartNew(() => ThresholdQuicksort(array, left, j, threshold));
-                    Parallel.Invoke(() => AsyncQuicksort(array, left, j));
-            if (i < right)
-                if ((right - i) < threshold)
-                    SyncQuicksort(array, i, right);
-                else
-                    //Task.Factory.StartNew(() => ThresholdQuicksort(array, i, right, threshold));
-                    Parallel.Invoke(() => AsyncQuicksort(array, i, right));
+
+            int size = right - left;
+            if (size < threshold)
+            {
+                SyncQuicksort(array, left, j);
+                SyncQuicksort(array, i, right);
+            }
+            else
+            {
+                // Erstelle Tasks für linke und rechte Partition
+                Task leftTask = (left < j) ? Task.Run(() => ThresholdQuicksortAsync(array, left, j, threshold)) : Task.CompletedTask;
+                Task rightTask = (i < right) ? Task.Run(() => ThresholdQuicksortAsync(array, i, right, threshold)) : Task.CompletedTask;
+
+                // Warte auf beide Tasks
+                await Task.WhenAll(leftTask, rightTask);
+            }
         }
         private static void SyncQuicksort(int[] array, int left, int right)
         {
@@ -107,58 +131,97 @@ namespace Quicksort_In_Place_Parallel
             if (i < right)
                 SyncQuicksort(array, i, right);
         }
-        static void Main(string[] args)
+
+
+        static void BenchmarkStatic(int[] array)
         {
-            int arraySize = 1000000;
-            int maxNumber = 10000000;
-            //int arraySize = 10;
-            //int maxNumber = 50;
-            //Array anlegen und befüllen
-            int[] array = new int[arraySize];
-            Random rnd = new Random();
-            for (int i = 0; i < array.Length; i++)
+            Console.WriteLine("--------- Static Benchmark START ---------\n\n\n");
+            int[] arrayToSort = new int[array.Length];
+            for (int j = 0; j < array.Length; j++)
             {
-                array[i] = rnd.Next(1, maxNumber);
+                arrayToSort[j] = array[j];
             }
-
-            int[] arrayToSort = new int[arraySize];
-            //for (int j = 0; j < array.Length; j++)
-            //{
-            //    arrayToSort[j] = array[j];
-            //}
-            //Stopwatch sw2 = new Stopwatch();
-            //sw2.Start();
-            //AsyncQuicksort(arrayToSort, 0, arrayToSort.Length - 1);
-            //sw2.Stop();
-            //Console.WriteLine("Naive ParallelQuicksort took: " + sw2.ElapsedMilliseconds + " Milli Seconds");
-            //Console.WriteLine("\nSortiertes Array:");
-            //Console.WriteLine(string.Join(", ", arrayToSort));
-            for (int i = 0; i < 100; i++)
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            SyncQuicksort(arrayToSort, 0, arrayToSort.Length - 1);
+            sw.Stop();
+            if (isSorted(arrayToSort))
             {
-                for (int j = 0; j < array.Length; j++)
+                Console.WriteLine("Static Quicksort took: " + sw.ElapsedMilliseconds + "ms");
+            }
+            else
+            {
+                Console.WriteLine("Static Quicksort did not sort correctly!!!");
+            }
+            Console.WriteLine("\n\n\n--------- Static Benchmark END ---------");
+        }
+
+        static async Task BenchmarkNaiveParallel(int[] array)
+        {
+            Console.WriteLine("--------- Naive Parallel Benchmark START ---------\n\n\n");
+            int[] arrayToSort = new int[array.Length];
+            for (int j = 0; j < array.Length; j++)
+            {
+                arrayToSort[j] = array[j];
+            }
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            await AsyncQuicksortAsync(arrayToSort, 0, arrayToSort.Length - 1);
+            sw.Stop();
+            if(isSorted(arrayToSort))
+            {
+                Console.WriteLine("Naive ParallelQuicksort took: " + sw.ElapsedMilliseconds + "ms");
+            }
+            else
+            {
+                Console.WriteLine("Naive ParallelQuicksort did not sort correctly!!!");
+            }
+            Console.WriteLine("\n\n\n--------- Naive Parallel Benchmark END ---------");
+        }
+
+        static async Task BenchmarkThresholdApproachAsync(int[] array)
+        {
+            Console.WriteLine("--------- Parallel with Threshold Benchmark START ---------\n\n\n");
+
+            string filePath = "ThresholdResults.csv";
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                writer.WriteLine("Threshold,Time(ms)");
+
+                for (int i = array.Length / 1000; i < array.Length / 3; i = i + array.Length / 1000)
                 {
-                    arrayToSort[j] = array[j];
+                    int[] arrayToSort = (int[])array.Clone();
+                    Stopwatch sw = new Stopwatch();
+                    int threshold = i;
+                    sw.Start();
+                    await ThresholdQuicksortAsync(arrayToSort, 0, arrayToSort.Length - 1, threshold);
+                    sw.Stop();
+
+                    long elapsedMs = sw.ElapsedMilliseconds;
+                    writer.WriteLine($"{threshold},{elapsedMs}");
+
+                    if (isSorted(arrayToSort))
+                    {
+                        Console.WriteLine($"ParallelQuicksort with Threshold <{threshold}> took: {elapsedMs}ms");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"ParallelQuicksort with Threshold <{threshold}> did not sort correctly!!!");
+                    }
                 }
-                //Console.WriteLine("Unsortiertes Array:");
-                //Console.WriteLine(string.Join(", ", arrayToSort));
-                Stopwatch sw1 = new Stopwatch();
-                int threshold = i;
-                sw1.Start();
-                ThresholdQuicksort(arrayToSort, 0, arrayToSort.Length - 1, threshold);
-                //SyncQuicksort(arrayToSort, 0, arrayToSort.Length - 1);
-                sw1.Stop();
-                //Console.WriteLine("\nSortiertes Array:");
-                Console.WriteLine("Threshold: "+threshold+" Time: " + sw1.ElapsedMilliseconds + "\n\n\n");
             }
-            Console.WriteLine("DONE");
 
-            //Console.WriteLine();
-            //Console.WriteLine();
-            //Console.WriteLine();
-            //Console.WriteLine();
-            //Console.WriteLine("\nSortiertes Array:");
-            //Console.WriteLine(string.Join(", ", array));
+            Console.WriteLine("\n\n\n--------- Parallel with Threshold Benchmark END ---------");
+            Console.WriteLine($"Results written to {filePath}");
+        }
 
+        static bool isSorted(int[] a)
+        {
+            for (int i = 1; i < a.Length; i++)
+            {
+                if (a[i - 1] > a[i]) return false;
+            }
+            return true;
         }
     }
 }
